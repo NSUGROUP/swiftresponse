0eb97ab14d1300c2183fbaa0468e8bcf29be3703
Elias <41076123+17specs@users.noreply.github.com>
2019-05-26 16:55:33 -0400

    Added Dashboard Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route} from 'react-router-dom';
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import Dashboard from "./components/pages/Dashboard";
 
 
 const App = () => <div class="ui container">
 
 <Route path="/" exact component={HomePage} />
 <Route path="/login" exact component={LoginPage} />
+<Route path="/dashboard" exact component={Dashboard} />
 </div>;
 
 export default App;
---
+++ src/components/pages/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Dashboard extends React.Component{
+
+    
+
+    render(){
+        return(
+            <div>
+            <h1>Main Dashboard</h1>
+
+            
+            </div>
+
+
+        );
+    }
+}
+
+
+
+export default Dashboard;
