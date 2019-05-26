c6d1873ff1d2a929fdbdd721b70a8bde1cb407a1
Hussain Khalil <hussainpilot05@gmail.com>
2019-05-26 17:55:53 -0400

    Fix server routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const port = 42069;
 
 const express = require('express');
+const path = require('path');
+const users = require('users.json');
 const app = express();
 
-app.use(express.static("build"));
+app.use("/", express.static("build"));
+app.use((req, res) => res.sendFile(path.resolve(__dirname, '../build/index.html')));
 
 app.listen(port, () => {console.log("Listening on port " + port)});
